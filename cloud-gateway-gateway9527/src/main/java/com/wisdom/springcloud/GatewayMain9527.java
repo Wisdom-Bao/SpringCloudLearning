5f4e6d8cef142f00b08928448d75d4d2996a02c2
WisdomBao <839928223@qq.com>
2020-09-15 14:53:15 +0800

    gateway

---
+++ cloud-gateway-gateway9527/src/main/java/com/wisdom/springcloud/GatewayMain9527.java
@@ -0,0 +1,18 @@
+package com.wisdom.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+/**
+ * @Author WisdomBao
+ * @Date 2020/9/15 10:44
+ * @Version 1.0
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class,args);
+    }
+}
---
+++ cloud-gateway-gateway9527/src/main/java/com/wisdom/springcloud/config/GatewayConfig.java
@@ -0,0 +1,13 @@
+package com.wisdom.springcloud.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author WisdomBao
+ * @Date 2020/9/15 11:14
+ * @Version 1.0
+ */
+
+@Configuration
+public class GatewayConfig {
+}
